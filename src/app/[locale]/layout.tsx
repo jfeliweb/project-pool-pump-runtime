0fe5ee0138eeed08fd05365ae760d125215984b1
Jean Felisme <jean@clockwork.com>
2025-12-08 13:15:46 -0500

    fix: toast on  calc latout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { PostHogProvider } from '@/components/analytics/PostHogProvider';
+import { ToastProvider } from '@/components/ui/Toast';
 import { routing } from '@/libs/I18nRouting';
 import '@/styles/global.css';
 
@@ -52,7 +53,9 @@ export default async function RootLayout(props: {
       <body>
         <NextIntlClientProvider>
           <PostHogProvider>
-            {props.children}
+            <ToastProvider>
+              {props.children}
+            </ToastProvider>
           </PostHogProvider>
         </NextIntlClientProvider>
       </body>
